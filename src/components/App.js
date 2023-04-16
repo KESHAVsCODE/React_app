b84f4def8e9324ed0d605f43f221d5ca393f9452
KESHAVsCODE <106483384+KESHAVsCODE@users.noreply.github.com>
2023-04-16 19:02:06 +0530

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import "../styles/App.css";
+
+export default function App() {
+    const [theme.setTheme] = useState("light");
+    const function = () =>{
+        if(theme === "light")setTheme("dark");
+        else setTheme("light");
+    }
+    return (
+        <div className="App" id = {theme === "light" ? "light" : "dark"} >
+            <h1>Newton School</h1>
+            <div>
+                <form>
+                    <label>UserName</label>
+                    <input></input>
+                    <label>Password</label>
+                    <input></input>
+                    <button>Login</button>
+                </form>
+            </div>
+            <label> {theme === "light" ? "Light Mode" : "Dark Mode"}</label>
+            <input type="checkbox" id="switch" />
+            <label for="switch" className="label">
+                Toggle
+            </label>
+        </div>
+    );
+}
